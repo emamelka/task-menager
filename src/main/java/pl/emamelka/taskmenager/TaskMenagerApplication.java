c31fdb84e2967a4938965de749ea8e6164baa604
emamelka <edyta.mamelka1@gmail.com>
2019-02-07 21:15:29 +0100

    Initial commit

---
+++ src/main/java/pl/emamelka/taskmenager/TaskMenagerApplication.java
@@ -0,0 +1,14 @@
+package pl.emamelka.taskmenager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskMenagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskMenagerApplication.class, args);
+	}
+
+}
+
